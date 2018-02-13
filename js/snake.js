fa6f6aed2e1e896deb40f2378c4caaf32caaf562
xyzCh <hsn940814@live.com>
2018-02-13 20:25:26 +0800

    Update snake.js

--- js/snake.js
+++ js/snake.js
@@ -9,7 +9,7 @@ var grid = (function () {
             var Direction = "w";
             var timer;
             var LostListener,EatListener;
-            var speed = 4;
+            var speed = 0;
             var isdone = true;
 
             var $ = function (id) {
